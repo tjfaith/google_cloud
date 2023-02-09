6b4da7dc6a5c2b724211b3a2e8ec5eee179628b9
tjfaithpro <tjfaithpro@gmail.com>
2023-02-09 19:33:54 +0100

    up

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('respond with a resource');
+  res.send('respond yuyyu');
 });
 
 export default router;
