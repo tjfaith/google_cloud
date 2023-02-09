53d29f58abd33e3f25c09c6571a8d1628908c933
tjfaithpro <tjfaithpro@gmail.com>
2023-02-09 19:49:37 +0100

    change port

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('home route')
+  res.send('home route hereee')
 });
 
 export default router;
