1b43ea0d94e871f7a961fcb38c5cc0628041b31e
tjfaithpro <tjfaithpro@gmail.com>
2023-02-10 17:30:07 +0100

    added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('this is the index route')
+  res.send('this is the index route 22222')
 });
 
 export default router;
