6be6068d7e23926898c16d56b5cbdf4d58e87b8e
tjfaithpro <tjfaithpro@gmail.com>
2023-02-10 12:50:33 +0100

    change users route content

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('respond yuyyu');
+  res.send('new users');
 });
 
 export default router;
