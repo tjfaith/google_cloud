f0bd782ec9f7847f13076436a2642576c60a01be
tjfaithpro <tjfaithpro@gmail.com>
2023-02-13 11:45:40 +0100

    update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req:Request, res:Response, next:NextFunction) {
-  res.send('this ')
+  res.send('this jdkjjskjjdd ')
 });
 
 export default router;
